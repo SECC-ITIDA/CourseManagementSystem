d68a8256bf47cfcd72419adf1323873398b773f3
Mustafa M. Gamal <mustafa@moveitgate.com>
2015-11-09 14:38:08 +0200

    Update Mongoose to version 4.2.4

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ var rootPath = path.normalize(__dirname + '/../');
 module.exports=
 	{
 		development:{
-			db:'mongodb://127.0.0.1:27017/cmsDb',
+			db:'mongodb://cmuser:pwd123@ds049624.mongolab.com:49624/cmdb', // mongodb://127.0.0.1:27017/cmsDb',
 			rootPath: rootPath,
 			port: process.env.port || 3000
 		},
--- config/mongoose.js
+++ config/mongoose.js
@@ -4,8 +4,8 @@ var mongoose =require('mongoose');
 module.exports= function(config){
 	mongoose.connect(config.db);
 	var db = mongoose.connection;
-	db.on('error', console.error.bind(console,'Connection error... '));
+	db.on('error', console.error.bind(console,'Connection error... ' + config.db));
 	db.once('open', function(){
-		console.log('cmsDb opened');
+		console.log('cmsDb opened'+ config.db);
 	});
 };
